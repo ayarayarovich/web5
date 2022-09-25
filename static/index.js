5704c3b0f64293559cfaf0f2cb0af921deaa439d
Айяр Халилов <super.ayar-04@yandex.ru>
2022-09-25 16:46:51 +0300

    Optimize background loading speed
    It seems better now on slow networks

--- static/index.js
+++ static/index.js
@@ -1,12 +1,14 @@
 
 window.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        const bgCanvas = document.querySelector("#bg-canvas");
-        bgCanvas.classList.add(`canvas--background-${Math.ceil(Math.random()*37)}`);
-    }, 1000);
-
+    const bgCanvas = document.querySelector("#bg-canvas");
     const calculatorFormEl = document.querySelector("#calculatorForm");
     const resultEl = document.querySelector("#calculator__result");
+
+    bgCanvas.classList.add(`canvas--background-${Math.floor(Math.random()*37)}`);
+
+    setTimeout(() => {
+        bgCanvas.classList.add('opacity-100');
+    }, 1500);
 
     calculatorFormEl.addEventListener("submit", (e) => {
         e.preventDefault();
