8f81c30e054f07a3c67559377f9afe0a9e4b8b30
Айяр Халилов <super.ayar-04@yandex.ru>
2022-09-26 09:32:26 +0300

    Make jslint.com shutup

--- static/index.js
+++ static/index.js
@@ -1,22 +1,24 @@
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", function() {
     const bgCanvas = document.querySelector("#bg-canvas");
     const calculatorFormEl = document.querySelector("#calculatorForm");
     const resultEl = document.querySelector("#calculator__result");
 
+    // JSLint ругается на длинну строки больше 80 символов.
+    // В этой строке 82 символа, я думаю это не критично =).
     bgCanvas.classList.add(`canvas--background-${Math.floor(Math.random()*37)}`);
 
-    setTimeout(() => {
-        bgCanvas.classList.add('opacity-100');
+    setTimeout(function() {
+        bgCanvas.classList.add("opacity-100");
     }, 1500);
 
-    calculatorFormEl.addEventListener("submit", (e) => {
+    calculatorFormEl.addEventListener("submit", function(e) {
         e.preventDefault();
 
         const formData = Object.fromEntries(new FormData(e.target).entries());
-        Object.keys(formData).forEach(key => {
+        Object.keys(formData).forEach(function(key) {
             formData[key] = parseInt(formData[key]);
-        })
+        });
 
         resultEl.value = formData.price * formData.amount;
     });
